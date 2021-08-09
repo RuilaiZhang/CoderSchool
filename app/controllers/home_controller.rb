2cc61c2ae6d1b2bbb38500b42a3d3e8bf465ac4e
Ruilai Zhang <ray.zhangruilai@gmail.com>
2021-08-09 13:14:58 +1000

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+    @api = StockQuote::Stock.new(api_key: 'pk_ced821cbeca946af844d67a5a2fde3c7')
+    if params[:ticker] == ""
+      @nothing = "You forgot to enter a symbol"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue StandardError
+        @error = "Hey! That stock symbol dosen't exist!"
+      end
+    end
+  end
+
+  def about
+  end
+end
